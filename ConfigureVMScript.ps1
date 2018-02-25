3618dbfe79f4c2d9c3d3a71787718d490b85fe7a
Evan Basalik <evanba@microsoft.com>
2018-02-25 17:19:44 -0500

    Simplify HTML coe

--- ConfigureVMScript.ps1
+++ ConfigureVMScript.ps1
@@ -5,7 +5,4 @@ New-NetFirewallRule -DisplayName "Allow Inbound Port 80" -Direction Inbound -Loc
 Add-WindowsFeature Web-Server
 
 #Create our HTML file for load balancing demonstration
-$TargetFile=c:\inetpub\wwwroot\default.html
-New-Item $TargetFile -Type File -Force
-$htmltext=$env:COMPUTERNAME
-$htmltext | Out-File $TargetFile
+$env:COMPUTERNAME | Out-File c:\inetpub\wwwroot\default.html -Force
