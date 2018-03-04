fea40d855f0b75d23ac8d147c3e742e4fb2b0223
Evan Basalik <evanba@microsoft.com>
2018-03-03 22:21:04 -0500

    Added comments

--- Lab2-P2S.ps1
+++ Lab2-P2S.ps1
@@ -17,6 +17,7 @@ if ($context.Environment -eq $null) {
 }
 
 #create the GW subnet
+Write-Host "Creating gateway subnet..." -ForegroundColor Yellow
 $vnet = Get-AzureRmVirtualNetwork -Name $VNetName -ResourceGroupName $resourceGroupName
 Add-AzureRmVirtualNetworkSubnetConfig -Name $GWSubName -VirtualNetwork $vnet -AddressPrefix $GWSubPrefix
 Set-AzureRmVirtualNetwork -VirtualNetwork $vnet
@@ -30,21 +31,25 @@ $pip = New-AzureRmPublicIpAddress -Name $GWIPName -ResourceGroupName $resourceGr
 $ipconf = New-AzureRmVirtualNetworkGatewayIpConfig -Name $GWIPconfName -Subnet $subnet -PublicIpAddress $pip
 
 #create the VNET gateway
+Write-Host "Creating gateway..." -ForegroundColor Yellow
 New-AzureRmVirtualNetworkGateway -Name $GWName -ResourceGroupName $resourceGroupName `
 	-Location $Location -IpConfigurations $ipconf -GatewayType Vpn `
 	-VpnType RouteBased -EnableBgp $false -GatewaySku VpnGw1 -VpnClientProtocol "IKEV2"
 
 #create the client IP pool
+Write-Host "Creating gateway client IP pool..." -ForegroundColor Yellow
 $Gateway = Get-AzureRmVirtualNetworkGateway -ResourceGroupName $resourceGroupName -Name $GWName
 Set-AzureRmVirtualNetworkGateway -VirtualNetworkGateway $Gateway -VpnClientAddressPool $VPNClientAddressPool
 
 #Create a self-signed root certificate for the gateway
+Write-Host "Creating root certificate..." -ForegroundColor Yellow
 $cert = New-SelfSignedCertificate -Type Custom -KeySpec Signature `
 	-Subject "CN=P2SRootCert" -KeyExportPolicy Exportable `
 	-HashAlgorithm sha256 -KeyLength 2048 `
 	-CertStoreLocation "Cert:\CurrentUser\My" -KeyUsageProperty Sign -KeyUsage CertSign
 
 #Generate a client certificate for the clients to present to the gateway
+Write-Host "Creating child certificate..." -ForegroundColor Yellow
 New-SelfSignedCertificate -Type Custom -DnsName P2SChildCert -KeySpec Signature `
 	-Subject "CN=P2SChildCert" -KeyExportPolicy Exportable `
 	-HashAlgorithm sha256 -KeyLength 2048 `
@@ -52,25 +57,29 @@ New-SelfSignedCertificate -Type Custom -DnsName P2SChildCert -KeySpec Signature 
 	-Signer $cert -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.2")
 
 #Export the certificate without the private key into a base-64 encoded CER
+Write-Host "Exporting certificate..." -ForegroundColor Yellow
 $cert = Get-Item -Path Cert:\CurrentUser\My\$($cert.Thumbprint)
 $content = @(
 '-----BEGIN CERTIFICATE-----'
 [System.Convert]::ToBase64String($cert.RawData, 'InsertLineBreaks')
 '-----END CERTIFICATE-----'
 )
-
 $content | Out-File -FilePath $filePathForCert -Encoding ascii
+Write-Host "Exported certificate to $($filePathForCert)" -ForegroundColor Green
 
 #upload the certificate to the gateway
+Write-Host "Uploading root certificate to gateway..." -ForegroundColor Yellow
 $cert = new-object System.Security.Cryptography.X509Certificates.X509Certificate2($filePathForCert)
 $CertBase64 = [system.convert]::ToBase64String($cert.RawData)
 Add-AzureRmVpnClientRootCertificate -VpnClientRootCertificateName $P2SRootCertName `
 	-VirtualNetworkGatewayname $GWName -ResourceGroupName $resourceGroupName -PublicCertData $CertBase64
 
 #download the P2S client files
+Write-Host "Downloading P2S client files..." -ForegroundColor Yellow
 $profile=New-AzureRmVpnClientConfiguration -ResourceGroupName $resourceGroupName -Name $GWName -AuthenticationMethod "EapTls"
 $url = $profile.VPNProfileSASUrl
 $output = "vpnclientconfiguration.zip"
 $wc=New-Object System.Net.WebClient
 $wc.DownloadFile($url, $output)
+Write-Host "Downloaded client files to $($output)" -ForegroundColor Green
 
