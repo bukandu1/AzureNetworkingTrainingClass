c253dc33044e69236524be4200e7d33e531252a0
Olga Osinskaya <ssinsk@yandex.ru>
2018-03-09 13:58:24 -0800

    Add files via upload

---
+++ lab2.ps1
@@ -0,0 +1,259 @@
+﻿##############################################################
+##Use this to login to your Azure account for the first time##
+##############################################################
+
+$login = Read-Host "Do you need to login Y/N"
+
+if($login -eq "Y")
+{
+Login-AzureRmAccount
+}
+
+else
+{
+Write-Host "You are logged in already" -ForegroundColor Green
+}
+
+###############################################
+##Checks for multi subscritptions selects one##
+###############################################
+
+$multisubs = Read-Host "Do you have multiple subscriptions Y/N"
+
+if($multisubs -eq "Y")
+{
+Get-AzureRmSubscription | Out-GridView
+
+$subscript = Read-Host "Enter the subscription you want to create these resources on"
+
+Set-AzureRmContext -Subscription $subscript
+}
+else 
+{
+Write-Host "Are you sure you dont have multiple subscriptions? Press stop if you do!" -ForegroundColor Cyan
+}
+
+###################################################
+##switch used to allow the selection of locations##
+###################################################
+
+Write-Host "
+ 1 for East Asia
+ 2 for Southeast Asia
+ 3 for Central US
+ 4 for East US
+ 5 for East US 2
+ 6 for West US
+ 7 for North Central US
+ 8 for South Central US
+ 9 for North Europe
+10 for West Europe
+11 for Japan West
+12 for Japan East
+13 for Brazil South
+14 for Australia East
+15 for Australia Southeast
+16 for South India
+17 for Central India
+18 for West India
+19 for Canada Central
+20 for Canada East
+21 for UK South
+22 for UK West
+23 for West Central US
+24 for West US 2
+25 for Korea Central
+26 for Korea South"; $number = Read-Host "Enter the coresponding number to select your location"
+
+switch($number)
+{
+1{$location = "eastasia"}
+2{$location = "southeastasia"}
+3{$location = "centralus"}
+4{$location = "eastus"}
+5{$location = "eastus2"}
+6{$location = "westus"}
+7{$location = "northcentralus"}
+8{$location = "southcentralus"}
+9{$location = "northeurope"}
+10{$location = "westeurope"}
+11{$location = "japanwest"}
+12{$location = "japaneast"}
+13{$location = "brazilsouth"}
+14{$location = "australiaeast"}
+15{$location = "australiasoutheast"}
+16{$location = "southindia"}
+17{$location = "centralindia"}
+18{$location = "westindia"}
+19{$location = "canadacentral"}
+20{$location = "canadaeast"}
+21{$location = "uksouth"}
+22{$location = "ukwest"}
+23{$location = "westcentralus"}
+24{$location = "westus2"}
+25{$location = "koreacentral"}
+26{$location = "koreasouth"}
+}
+
+################################################
+##switch allowing the selection of your images##
+################################################
+
+Write-Host "
+ 1 for CentOS
+ 2 for CoreOS
+ 3 for Debian
+ 4 for openSUSE-Leap
+ 5 for RHEL
+ 6 for SLES
+ 7 for UbuntuLTS
+ 8 for Win2016Datacenter
+ 9 for Win2012R2Datacenter
+10 for Win2012Datacenter
+11 for Win2008R2SP1"; $switchnum = Read-Host "Enter the coresponding number to select your image"
+
+switch($switchnum)
+{
+1{$image = "CentOS"}
+2{$image = "CoreOS"}
+3{$image = "Debian"}
+4{$image = "openSUSE-Leap"}
+5{$image = "RHEL"}
+6{$image = "SLES"}
+7{$image = "UbuntuLTS"}
+8{$image = "Win2016Datacenter"}
+9{$image = "Win2012R2Datacenter"}
+10{$image = "Win2012Datacenter"}
+11{$image = "Win2008R2SP1"}
+}
+
+###########################################
+##Variables to allow reuse of this script##
+###########################################
+
+$VMname = Read-Host "Enter the name you would like for your vm/s"
+
+$MyVnet = Read-Host "Enter the name for your Virtual Network"
+
+#$Subname = Read-Host "Enter your Subnet name"
+
+$NSG = Read-Host "Enter your Network Security Group name"
+
+#$PublicIP = Read-Host "Enter your public IP name"
+
+$OpenPs = Read-Host "Enter the ports you want open if any 3389, 80 etc."
+
+$OpenPs = [int32]$OpenPs
+
+################################################################
+##If statements to take care of some of the default selections##
+################################################################
+
+$IPDefault = Read-Host "Do you want to keep the default IP length Y/N"
+
+if($IPDefault -eq "N")
+{
+$IPlength = Read-Host "Enter desired IP prefix length"
+}
+else
+{
+$IPlength = "192.168.0.0/16"
+}
+
+$DefaultSize = Read-Host "Do you want the Default machine size etc Standard_DS1_v2 Y/N"
+
+if($DefaultSize -eq "N")
+{
+$Size = Read-Host "Enter the Virtual machine size default is Standard_DS1_v2"
+}
+else
+{
+$Size = "Standard_DS1_v2"
+}
+
+$DefaultSub = Read-Host "Do you want the Default value for your subnet length Y/N"
+
+if($DefaultSub -eq "N")
+{
+$subnetprefix = Read-Host "Enter the prefix length for you would like for your subnet ensure it is smaller than your IP length"
+}
+else
+{
+$subnetprefix = "192.168.1.0/24"
+}
+
+####################################
+##If you want a new resource group##
+####################################
+
+Get-AzureRmResourceGroup | Out-GridView
+
+$resoure = Read-Host "Do you need a resource group Y/N"
+
+if ($resoure -eq "Y")
+{
+$resname = Read-Host "Enter the name you would like for your resource group"
+
+New-AzureRmResourceGroup -Name $resname -Location $location
+}
+else
+{
+$resname = Read-Host "Enter the name of the resource group you are creating this VM on"
+}
+
+########################
+##New Availability set##
+########################
+
+$available = Read-Host "Do you need an Availability Set Y/N"
+
+if($available -eq "Y")
+{
+
+$managed = Read-Host "Managed Y/N"
+
+if($managed -eq "Y")
+{
+$availability = Read-Host "Enter availability set name"
+
+$updatedomain = Read-Host "Enter your desired update domain count 5 is default max is 20"
+
+New-AzureRmAvailabilitySet -ResourceGroupName $resname -Name $availability -Location $location -Managed -PlatformFaultDomainCount 2 -PlatformUpdateDomainCount $updatedomain
+
+}
+
+else
+{
+$availability = Read-Host "Enter availability set name"
+
+$updatedomain = Read-Host "Enter your desired update domain count 5 is default max is 20"
+
+New-AzureRmAvailabilitySet -ResourceGroupName $resname -Name $availability -Location $location -PlatformFaultDomainCount 2 -PlatformUpdateDomainCount $updatedomain
+}
+
+}
+
+else
+{
+Write-Host "You selected No availability set" -ForegroundColor Red
+}
+
+#################
+##Create new VM##
+#################
+
+$t = 1
+
+$number = Read-Host "Enter number of Vms you want"
+
+$number = [int64]$number
+
+while($t -ile $number)
+{
+
+New-AzureRmVM -ResourceGroupName $resname -Name $VMname$t -Location $location -VirtualNetworkName $MyVnet `
+-SubnetName $MyVnet -SecurityGroupName $NSG -PublicIpAddressName $MyVnet$t -OpenPorts $OpenPs -ImageName $image `
+-AvailabilitySetName $availability -AddressPrefix $IPlength -Size $Size -SubnetAddressPrefix $subnetprefix 
+
+$t++
+}
---
+++ labFriday.ps1
@@ -0,0 +1,175 @@
+﻿$nsgName="<your value here>"
+$resourceGroupName="<your value here>"
+$location="West Europe"
+$tags=(Get-AzureRmResourceGroup -Name <your value here>).Tags
+
+
+
+#check if we need to log in
+$context =  Get-AzureRmContext
+if ($context.Environment -eq $null) {
+    Login-AzureRmAccount
+}
+
+Write-Host "Creating resource group..." -ForegroundColor Yellow
+Set-AzureRmResourceGroup `
+-Name $resourceGroupName `
+-Tag $tags
+#-Location $location
+
+#add NSG rule to allow port 80 inbound from anywhere
+#Write-Host "Creating NSG..." -ForegroundColor Yellow
+#New-AzureRmNetworkSecurityGroup -Name $nsgName -ResourceGroupName $resourceGroupName -Location $location
+
+Write-Host "Allowing HTTP..." -ForegroundColor Yellow
+$nsg=Get-AzureRmNetworkSecurityGroup -Name $nsgName -ResourceGroupName $resourceGroupName | `
+    Add-AzureRmNetworkSecurityRuleConfig -Name "Allow_Port_80" -Access `
+    Allow -Protocol Tcp -Direction Inbound -Priority 110 -SourceAddressPrefix * `
+    -SourcePortRange * -DestinationAddressPrefix VirtualNetwork -DestinationPortRange 80 |`
+    Set-AzureRmNetworkSecurityGroup 
+
+Write-Host "Allowing RDP..." -ForegroundColor Yellow
+$nsg=Get-AzureRmNetworkSecurityGroup -Name $nsgName -ResourceGroupName $resourceGroupName | `
+    Add-AzureRmNetworkSecurityRuleConfig -Name "Allow_RDP" `
+    -Access Allow -Protocol Tcp -Direction Inbound -Priority 120 `
+    -SourceAddressPrefix Internet -SourcePortRange * `
+    -DestinationAddressPrefix * -DestinationPortRange 3389 | `
+    Set-AzureRmNetworkSecurityGroup 
+
+
+#VNETs
+Write-Host "Creating VNETs..." -ForegroundColor Yellow
+for ($i = 0; $i -lt 2; $i++) {
+    $NetworkName = "VNET$($i)"
+    $SubnetName = "Subnet1"
+    $SubnetAddressPrefix = "10.$($i).0.0/25"
+    $VnetAddressPrefix = "10.$($i).0.0/23"
+    $SingleSubnet = New-AzureRmVirtualNetworkSubnetConfig -Name $SubnetName -AddressPrefix $SubnetAddressPrefix
+
+    Write-Host "Creating $($NetworkName)..." -ForegroundColor Yellow
+    $Vnet = New-AzureRmVirtualNetwork -Name $NetworkName -ResourceGroupName $ResourceGroupName -Location $Location -AddressPrefix $VnetAddressPrefix -Subnet $SingleSubnet
+
+    ##add the NSG
+    Write-Host "Adding NSG to $($NetworkName)..." -ForegroundColor Yellow
+    Set-AzureRmVirtualNetworkSubnetConfig -VirtualNetwork $Vnet -Name $SubnetName -AddressPrefix $SubnetAddressPrefix -NetworkSecurityGroup $nsg
+    Set-AzureRmVirtualNetwork -VirtualNetwork $Vnet
+}
+
+#credentials for all the VMs
+$cred = Get-Credential -Message "Enter information for the local administrator on all VMs"
+
+#create availability set for the load balanced VMs in VNET0
+Write-Host "Adding availability set..." -ForegroundColor Yellow
+$as = New-AzureRmAvailabilitySet -ResourceGroupName $resourceGroupName -Name "Lab1AvailabilitySet" -Location $location `
+    -Sku aligned `
+    -PlatformFaultDomainCount 2 `
+    -PlatformUpdateDomainCount 2
+
+##two VMs in VNET0
+for ($i = 0; $i -lt 2; $i++) {
+
+    #names, etc.
+    $ComputerName = "Lab1VM$($i)"
+    Write-Host "Creating $($ComputerName)..." -ForegroundColor Yellow
+
+    $VMName = "Lab1VM$($i)"
+    $VMSize = "Standard_B2ms"
+    $NICName = "Lab1VM$($i)-NIC"
+    $PublicIPAddressName = "Lab1VM$($i)-PIP"
+
+    $Vnet = Get-AzureRmVirtualNetwork -Name  "VNET0" -ResourceGroupName $resourceGroupName
+    $PIP = New-AzureRmPublicIpAddress -Name $PublicIPAddressName -ResourceGroupName $resourceGroupName -Location $location -DomainNameLabel "$($resourceGroupName)PIP$($i)".ToLower() -AllocationMethod Static
+    $NIC = New-AzureRmNetworkInterface -Name $NICName -ResourceGroupName $resourceGroupName -Location $location -SubnetId $Vnet.Subnets[0].Id -PublicIpAddressId $PIP.Id
+
+    # Create a virtual machine configuration
+    $vmConfig = New-AzureRmVMConfig -VMName $VMName -VMSize $VMSize -AvailabilitySetId $as.Id | `
+    Set-AzureRmVMOperatingSystem -Windows -ComputerName $VMName -Credential $cred | `
+    Set-AzureRmVMSourceImage -PublisherName MicrosoftWindowsServer -Offer WindowsServer `
+    -Skus 2016-Datacenter -Version latest | Add-AzureRmVMNetworkInterface -Id $NIC.Id
+
+    #create the VM
+    New-AzureRmVM -ResourceGroupName $resourceGroupName -Location $location -vm $vmConfig -Verbose
+    Write-Host "Created $($ComputerName)..." -ForegroundColor Green
+}
+
+##one VM in VNET1
+#names, etc.
+$ComputerName = "Lab1VM2"
+Write-Host "Creating $($ComputerName)..." -ForegroundColor Yellow
+
+$VMName = $ComputerName
+$VMSize = "Standard_B2ms"
+$NICName = "$($ComputerName)-NIC"
+$PublicIPAddressName = "$($ComputerName)-PIP"
+
+$Vnet = Get-AzureRmVirtualNetwork -Name  "VNET1" -ResourceGroupName $resourceGroupName
+$PIP = New-AzureRmPublicIpAddress -Name $PublicIPAddressName -ResourceGroupName $resourceGroupName -Location $location -DomainNameLabel "$($resourceGroupName)PIP2".ToLower() -AllocationMethod Static
+$NIC = New-AzureRmNetworkInterface -Name $NICName -ResourceGroupName $resourceGroupName -Location $location -SubnetId $Vnet.Subnets[0].Id -PublicIpAddressId $PIP.Id
+
+# Create a virtual machine configuration
+$vmConfig = New-AzureRmVMConfig -VMName $VMName -VMSize $VMSize | `
+Set-AzureRmVMOperatingSystem -Windows -ComputerName $VMName -Credential $cred | `
+Set-AzureRmVMSourceImage -PublisherName MicrosoftWindowsServer -Offer WindowsServer `
+-Skus 2016-Datacenter -Version latest | Add-AzureRmVMNetworkInterface -Id $NIC.Id
+
+#create the VM
+New-AzureRmVM -ResourceGroupName $resourceGroupName -Location $location -VM $vmConfig -Verbose
+Write-Host "Created $($ComputerName)..." -ForegroundColor Green
+
+#install IIS, configure firewall, create HTML file on all three VMs
+Write-Host "Configuring the VMs for Lab1..." -ForegroundColor Yellow
+for ($i = 0; $i -lt 3; $i++) {
+    $VMName = "Lab1VM$($i)"
+    Write-Host "Configuring $($VMName)..." -ForegroundColor Yellow
+
+   ##call the custom script extension to configure the VMs
+   Set-AzureRmVMCustomScriptExtension -ResourceGroupName $resourceGroupName `
+    -VMName $VMName `
+    -Location $location `
+    -FileUri https://raw.githubusercontent.com/EvanBasalik/AzureNetworkingTrainingClass/master/ConfigureVMScript.ps1 `
+    -Run 'ConfigureVMScript.ps1' `
+    -Name "Lab1Prep"
+}
+
+Write-Host "All VMs for Lab1 created and configured" -ForegroundColor Green
+
+##write out the web server URLs and RDP files
+for ($i = 0; $i -lt 3; $i++) {
+    $VMName = "Lab1VM$($i)"
+
+    #Get the public IP
+    $vm=Get-AzureRMVM -ResourceGroupName $resourceGroupName -Name $VMName
+    $VNIC = Get-AzureRmNetworkInterface | Where-Object {$_.Id -eq $vm.NetworkProfile.NetworkInterfaces[0].Id}
+    $EIPPublicIP = Get-AzureRmPublicIpAddress | Where-Object {$_.Id -eq $VNIC.IpConfigurations[0].PublicIpAddress.Id}
+
+    Write-Host "URL for $($VMName) is http://$($EIPPublicIP.IpAddress)/default.html" -ForegroundColor Green
+
+    ##generate the RDP files
+    Write-Host "Generating RDP file for $($VMName): " -ForegroundColor Yellow -NoNewline
+    Write-Host "$($VMName).rdp" -ForegroundColor Green
+    $rdpFile = "$($VMName).rdp"
+    "full address:s:$($EIPPublicIP.IpAddress):3389" | Out-File $rdpFile -Force
+    "prompt for credentials:i:1" | Out-File $rdpFile -Append
+    "administrative session:i:1" | Out-File $rdpFile -Append
+}
+
+$OUTPUT= [System.Windows.Forms.MessageBox]::Show("Please go create your ILB and ELB in the portal. Click OK when done." , `
+    "Wait for load balancers" , [System.Windows.Forms.MessageBoxButtons]::OK)
+#Add an ILB in VNET1 via the portal -> Choose your availability set (Lab1AvailabilitySet by default) and use TCP 80
+#Add an ELB in VNET1 via the portal -> same as above
+
+$OUTPUT= [System.Windows.Forms.MessageBox]::Show("While you are in the portal, create your shutdown schedules. Click OK when done." , `
+    "Wait for shutdown schedule" , [System.Windows.Forms.MessageBoxButtons]::OK)
+#Add the shutdown schedule
+
+#Create the Traffic Manager profile
+$TMprofile = New-AzureRmTrafficManagerProfile -Name "Lab1TM" -ResourceGroupName $resourceGroupName `
+    -TrafficRoutingMethod Weighted -RelativeDnsName $resourceGroupName -Ttl 5 -MonitorProtocol HTTP -MonitorPort 80 -MonitorPath "/"
+
+#Add the endpoints
+for ($i = 0; $i -lt 3; $i++) {
+    $PIPName = "Lab1VM$($i)-PIP"
+    $ip = Get-AzureRmPublicIpAddress -Name $PIPName -ResourceGroupName $resourceGroupName
+    New-AzureRmTrafficManagerEndpoint -Name "$($PIPName)-TMEndpoint" -ProfileName $TMprofile.Name `
+        -ResourceGroupName $resourceGroupName -Type AzureEndpoints -TargetResourceId $ip.Id -EndpointStatus Enabled 
+}
